b63880a53cebd12e975e1ad10fd68e2628744740
Hugo Melgarejo <108104330+hugomelgarejoo@users.noreply.github.com>
2022-11-05 11:27:50 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+function createGame(player1, hour, player2){
+    return `
+    <li>
+        <img src="assets/flags/icon-${player1}.svg" alt="Bandeira ${player1} ">
+        <strong>${hour} </strong>
+        <img src="assets/flags/icon-${player2}.svg" alt="Bandeira ${player2}">
+    </li>
+    `
+}
+
+let delay = -0.4;
+function createCard(date, day, games) {
+    delay = delay + 0.4;
+  return `
+    <div class="card" style="animation-delay: ${delay}s">
+        <h2>${date} <span>${day} </span></h2>
+        <ul>
+           ${games}
+        </ul>
+    </div>
+    `
+}
+
+document.querySelector("#cards").innerHTML =
+ 
+    createCard('24/11', 'quinta',
+    createGame('brazil', '16:00', 'serbia'))
+    +
+
+    createCard('28/11', 'segunda',
+    createGame('brazil', '13:00', 'switzerland') +
+    createGame('brazil', '13:00', 'switzerland'))
+    +
+
+    createCard('24/11', 'quinta',
+    createGame('brazil', '16:00', 'serbia'))
+
